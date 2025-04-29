0151c4e036dbe98d2524826db7416b1d0e7ffd3a
zunairasarbulandali <zunairasarbulandali@gmail.com>
2025-04-29 16:57:50 +0500

    Add files via upload

---
+++ grade.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+ int main ( ){ 
+// GRADE SYSTEM:
+int num;
+cout<< "Enter a num:" <<endl;
+cin>>num;
+if( num>=90)
+cout<< "Grade A" <<endl;
+if( num>=70)
+cout<< "Grade B" <<endl;
+if( num>=50)
+cout<< "Grade C" <<endl;
+else
+cout<<  "Fail" <<endl;
+
+
+
+
+     return 0;
+     }
