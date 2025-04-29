3619c38f67684050f8e8a5574fb9038ac1359e9e
zunairasarbulandali <zunairasarbulandali@gmail.com>
2025-04-29 16:59:59 +0500

    Add files via upload

---
+++ number-sign-check.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter a number:"<<endl;
+    cin>>num;
+    if(num>0)
+    cout<<"Positive"<<endl;
+else if(num<0)
+    cout<<"Negative"<<endl;
+else
+    cout<<"zero"<<endl;
+return 0;
+
+
+
+
+}
