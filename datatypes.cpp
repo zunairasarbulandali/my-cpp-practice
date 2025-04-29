c9b4991f6eea73ab9c8e77f5c094588c18e77ee8
zunairasarbulandali <zunairasarbulandali@gmail.com>
2025-04-29 16:53:22 +0500

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a=4;
+    int b=5;
+    char c='d';
+    cout<<"\n here the value of a is:"<<a;
+    cout<<"\n the value of b is:"<<b;
+    cout<<"\n the value of c is: "<<c;
+    return 0;
+}
