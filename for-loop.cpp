92026224db5d93d8d856e570fc4a2f0567c16846
zunairasarbulandali <zunairasarbulandali@gmail.com>
2025-05-06 14:56:22 +0500

    Add files via upload

---
+++ for-loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+ /*  cout<< "Print numbers from 1 to 10" <<endl;
+    for(int i=1; i<=10; i++){
+        cout<<i<<endl;
+    }*/
+    cout<< "Print numbers from 10 to 1"<<endl;
+    for(int i=10; i>=1; i--){
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
