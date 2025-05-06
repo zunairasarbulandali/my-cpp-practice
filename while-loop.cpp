3921844a9a597879eea8b355b7dc4ed181ca1f10
zunairasarbulandali <zunairasarbulandali@gmail.com>
2025-05-06 14:57:48 +0500

    Add files via upload

---
+++ while-loop.cpp
@@ -0,0 +1,14 @@
+//WHILE LOOP*******
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Print numbers from 1 to 10:"<<endl;
+    int i=1;
+    while(i<=10){
+    cout<<i<<endl;
+    i++;
+    }
+return 0;
+
+    
+}
