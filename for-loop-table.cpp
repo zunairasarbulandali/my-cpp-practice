104847c24101b0d7e48bc3a111c49afd16d7bc30
zunairasarbulandali <zunairasarbulandali@gmail.com>
2025-05-06 14:57:15 +0500

    Add files via upload

---
+++ for-loop-table.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    /*int num;
+    cout<< "Multiplication table :" <<endl;
+    cin>>num;
+    for(int i=1; i<=10; i++){
+        cout<< num << "x" << i << "=" << num*i <<endl; 
+  }*/
+  int num;
+  cout<< "Table of 3 is:" <<endl;
+  cin>>num;
+  for(int i=1; i<=10; i++){
+    cout<< num  << "x" << i << "=" <<num*i <<endl;
+  }
+
+  return 0;
+}
